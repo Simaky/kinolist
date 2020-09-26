8ccd07d5ece65b8b2b9a58486358df0634470a46
Simaky <maxympritika@gmail.com>
2020-09-27 01:00:58 +0300

    Add Film and User model/repository

---
+++ src/main/java/me/simaky/kinolist/model/User.java
@@ -0,0 +1,22 @@
+package me.simaky.kinolist.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "user")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+}
