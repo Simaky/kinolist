027c216871ee622e577f3f7b21ccc7f626fb4c77
Simaky <maxympritika@gmail.com>
2020-09-27 01:28:36 +0300

    Add healthCheck endpoint

---
+++ src/main/java/me/simaky/kinolist/controller/HealthCheckController.java
@@ -0,0 +1,12 @@
+package me.simaky.kinolist.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "ok";
+    }
+}
