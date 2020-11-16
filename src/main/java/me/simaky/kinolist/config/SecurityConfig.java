6d229b44e92309c926cbe38aaf213dfddbd6bfc1
Simaky <maxympritika@gmail.com>
2020-11-16 19:09:32 +0200

    Show list for unauthorized users, fix migration

--- src/main/java/me/simaky/kinolist/config/SecurityConfig.java
+++ src/main/java/me/simaky/kinolist/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/login", "/registration")
+                .antMatchers("/login", "/registration", "/users/{id}/films", "/users/{id}")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
